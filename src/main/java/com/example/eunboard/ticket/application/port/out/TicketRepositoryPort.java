04c6dbfee4e617b2e568a96e796484cdb5b93a6f
a60156077 <dwrhksfl77@naver.com>
2022-12-05 10:59:40 +0900

    ticket 통합

--- src/main/java/com/example/eunboard/ticket/application/port/out/TicketRepositoryPort.java
+++ src/main/java/com/example/eunboard/ticket/application/port/out/TicketRepositoryPort.java
@@ -9,6 +9,7 @@ import java.util.Optional;
 public interface TicketRepositoryPort {
     Optional<Ticket> findById(Long id);
     Ticket save(Ticket ticket);
+
     Ticket findByMember(Member member);
     List<Ticket> findByMemberId(Long memberId);
     List<Ticket> findAll();
