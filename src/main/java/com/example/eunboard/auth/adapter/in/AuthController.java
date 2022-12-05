353a04420573e30d2dd22816a537cc791bee0b3b
a60156077 <dwrhksfl77@naver.com>
2022-12-05 13:30:13 +0900

    AuthController swagger 적용

--- src/main/java/com/example/eunboard/auth/adapter/in/AuthController.java
+++ src/main/java/com/example/eunboard/auth/adapter/in/AuthController.java
@@ -1,11 +1,16 @@
 package com.example.eunboard.auth.adapter.in;
 
-import com.example.eunboard.member.application.port.in.LoginRequestDto;
 import com.example.eunboard.auth.application.port.in.TokenDto;
 import com.example.eunboard.auth.application.port.in.TokenRequestDto;
 import com.example.eunboard.auth.application.port.in.TokenUseCase;
+import com.example.eunboard.member.application.port.in.LoginRequestDto;
 import com.example.eunboard.member.application.port.in.MemberRequestDTO;
-import com.example.eunboard.member.application.port.in.MemberResponseDTO;
+import com.example.eunboard.shared.exception.ErrorResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +29,11 @@ import java.util.Map;
 public class AuthController {
     private final TokenUseCase loginService;
 
+    @Operation(summary = "회원가입", description = "회원가입을 진행합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "회원가입 성공"),
+            @ApiResponse(responseCode = "400", description = "회원가입 실패",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))) })
     @PostMapping("/signup")
     public ResponseEntity<Object> signup(@RequestBody @Valid MemberRequestDTO memberRequestDTO){
         loginService.signup(memberRequestDTO);
@@ -33,16 +43,27 @@ public class AuthController {
         return ResponseEntity.ok(map);
     }
 
+    @Operation(summary = "로그인", description = "로그인을 진행합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "로그인 성공", content = @Content(schema = @Schema(implementation = TokenDto.class))),
+            @ApiResponse(responseCode = "400", description = "로그인 실패", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
     @PostMapping("/login")
     public ResponseEntity<TokenDto> login(@RequestBody @Valid LoginRequestDto loginRequestDto){
         return ResponseEntity.ok(loginService.login(loginRequestDto));
     }
 
+    @Operation(summary = "토근재발급", description = "토큰을 재발급합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "발급 성공", content = @Content(schema = @Schema(implementation = TokenDto.class))),
+            @ApiResponse(responseCode = "400", description = "발급 실패", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
     @PostMapping("/reissue")
     public ResponseEntity<TokenDto> reissue(@RequestBody TokenRequestDto tokenRequestDto){
         return ResponseEntity.ok(loginService.reissue(tokenRequestDto));
     }
 
+    @Operation(summary = "로그아웃", description = "로그아웃을 진행합니다.")
     @PostMapping("/logout")
     public ResponseEntity<String> logout(@RequestBody TokenRequestDto tokenRequestDto){
         return ResponseEntity.ok(loginService.logout(tokenRequestDto));
