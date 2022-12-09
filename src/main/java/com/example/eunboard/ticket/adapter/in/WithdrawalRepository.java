1923c5ba5619af9cfcfa8a62ce0fcb6b4770857e
a60156077 <dwrhksfl77@naver.com>
2022-12-09 14:00:52 +0900

    날짜 로직 수정

--- src/main/java/com/example/eunboard/ticket/adapter/in/WithdrawalRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.eunboard.ticket.adapter.in;
-
-import com.example.eunboard.old.domain.entity.Withdrawal;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WithdrawalRepository extends JpaRepository<Withdrawal, Long> {
-}
