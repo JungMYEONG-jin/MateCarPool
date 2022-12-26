a0b9dc91d602b5e48522f108dca7fce5e0792a75
godcoder <dwrhksfl77@naver.com>
2022-12-26 23:47:35 +0900

    US-10, 새 문서에 맞게 변경

--- src/main/java/com/example/eunboard/member/application/port/in/ProfileResponseDto.java
+++ src/main/java/com/example/eunboard/member/application/port/in/ProfileResponseDto.java
@@ -1,6 +1,7 @@
 package com.example.eunboard.member.application.port.in;
 
 import com.example.eunboard.member.domain.Member;
+import com.example.eunboard.member.domain.MemberRole;
 import com.example.eunboard.ticket.application.port.in.TicketShortResponseDto;
 import com.example.eunboard.ticket.domain.Ticket;
 import com.example.eunboard.timetable.application.port.in.MemberTimetableResponseDTO;
@@ -17,16 +18,24 @@ import java.util.stream.Collectors;
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
 @AllArgsConstructor
 public class ProfileResponseDto {
-    private String department;
+    private String profileImage;
     private String memberName;
+    private String studentNumber;
+    private String department;
+    private String phoneNumber;
     private List<MemberTimetableResponseDTO> memberTimeTable = new ArrayList<>();
+    private MemberRole memberRole;
     private List<TicketShortResponseDto> tickets = new ArrayList<>();
 
     public static ProfileResponseDto of(Member member, List<Ticket> tickets){
         return ProfileResponseDto.builder()
-                .department(member.getDepartment())
+                .profileImage(member.getProfileImage())
                 .memberName(member.getMemberName())
+                .studentNumber(member.getStudentNumber())
+                .department(member.getDepartment())
+                .phoneNumber(member.getPhoneNumber())
                 .memberTimeTable(member.getMemberTimeTableList()!=null?member.getMemberTimeTableList().stream().map(MemberTimetableResponseDTO::toDTO).collect(Collectors.toList()):new ArrayList<>())
+                .memberRole(member.getAuth())
                 .tickets(tickets!=null?tickets.stream().map(TicketShortResponseDto::toDTO).collect(Collectors.toList()):new ArrayList<>())
                 .build();
     }
